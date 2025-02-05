d994733177aca81bb19f76062ce43b81a4c02fed
Raheem Weldon Jr <raheemweldonjr@Raheems-MacBook-Pro.local>
2025-02-04 23:30:45 -0500

    Encoder function

---
+++ CaesarEncoder.py
@@ -0,0 +1,19 @@
+
+def encoder(phrase, shiftNumber):
+    words = phrase.split()
+    updated_words =[]
+   
+    for word in words:
+        concat_word = ""
+        for c in word:
+            updated_c = ord(c) + shiftNumber
+            concat_word += chr(updated_c)
+        updated_words.append(concat_word)
+    encoded_words= " ".join(updated_words)
+    print (encoded_words)
+    return encoded_words
+
+
+        
+
+         
