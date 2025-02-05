6a84275ad8daa48a9510b7a502b4d1a8ed49f927
Raheem Weldon Jr <raheemweldonjr@Raheems-MacBook-Pro.local>
2025-02-04 23:37:51 -0500

    updated user abilities

--- CaesarMain.py
+++ CaesarMain.py
@@ -1,13 +1,23 @@
 import CaesarInputPrompter as cip 
 import CaesarEncoder as ce
+import CaesarDecoder as cd
+import sys
 global shiftNumber
 
 def main():
-    print("Welcome to Caesar-Cipher!")
-    phrase = cip.phrase_obtainer()
-    shiftnumber = cip.cc_shift_number()
-    ce.encoder(phrase,shiftnumber)
-    
-      
+    user_response = input("Welcome to Caesar-Cipher! If you would like to encode type 'encode' if you would like to decode type 'decode'")
+    user_response = user_response.lower()
+    if user_response == "encode":
+        phrase = cip.phrase_obtainer()
+        shiftnumber = cip.cc_shift_number()
+        encoded_phrase = ce.encoder(phrase,shiftnumber)
+        print("Encoded Phrase: ", encoded_phrase)
+    if user_response == "decode":
+        pass
+    else:
+        print("Incorrect input, Goodbye!")
+        sys.exit
+        
+        
 if __name__ == '__main__':
     main()
