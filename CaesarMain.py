b75533f0b967369ff49f2ca5ffdead6ef5bc941c
Raheem Weldon Jr <raheemweldonjr@Raheems-MacBook-Pro.local>
2025-02-04 23:44:16 -0500

    formatting changes

--- CaesarMain.py
+++ CaesarMain.py
@@ -5,18 +5,20 @@ import sys
 global shiftNumber
 
 def main():
-    user_response = input("Welcome to Caesar-Cipher! If you would like to encode type 'encode' if you would like to decode type 'decode'")
+    user_response = input("Welcome to Caesar-Cipher! If you would like to encode type 'encode' if you would like to decode type 'decode'\n")
     user_response = user_response.lower()
+    
     if user_response == "encode":
         phrase = cip.phrase_obtainer()
         shiftnumber = cip.cc_shift_number()
         encoded_phrase = ce.encoder(phrase,shiftnumber)
         print("Encoded Phrase: ", encoded_phrase)
+        sys.exit
     if user_response == "decode":
         pass
-    else:
-        print("Incorrect input, Goodbye!")
-        sys.exit
+
+    print("Incorrect input, Goodbye!")
+    sys.exit
         
         
 if __name__ == '__main__':
